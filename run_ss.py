11ae3481fd72d348987f51ddd4871185c159a91d
DaleyDeng <daleydeng@gmail.com>
2017-11-17 07:11:43 -0600

    Add files via upload

---
+++ run_ss.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+from os import system
+
+port = 22212
+password = "u6tFhWSbN2k9"
+servers = [
+    'hk1',
+    'jp1',
+    'jp1',
+    'us1',
+    'us2',
+]
+
+enc='chacha20-ietf-poly1305'
+
+if __name__ == "__main__":
+    from argparse import ArgumentParser
+    ap = ArgumentParser()
+    ap.add_argument('-s', type=int, default=0)
+    args = ap.parse_args()
+
+    print(servers[args.s])
+    system(f'sslocal -s {servers[args.s]}-sta13.3fufr.pw -p {port} -b 127.0.0.1 -l 1080 -k {password} -m {enc}')
